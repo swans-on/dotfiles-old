c6bb3edfdf7e599bc3c55cd9ae296384664e8bca
spaulder <mugen@machine.local>
2019-08-11 22:53:28 -0500

    added polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shutdown
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+polybar bar &
+
+echo "Bars launched..."
